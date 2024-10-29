77b978638c9c8a6a37b0e28f03bbd22b5397d35c
Amanda Delgado <amandadelgado1022@gmail.com>
2024-10-29 09:41:53 -0400

    updating with code for chart build.

--- static/js/app.js
+++ static/js/app.js
@@ -27,29 +27,62 @@ function buildCharts(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // Get the samples field
-
+    let samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
-
+    let sampleDAtaArray = samples.filter(sampleObj => sampleObj.id == sample);
+    let sampleData = sampleDataArray[0];
 
     // Get the otu_ids, otu_labels, and sample_values
-
+    let otu_ids = sampleData.otu_ids;
+    let otu_lables = sampleData.otu_lables;
+    let sample_values = sampleData.sample_values;
 
     // Build a Bubble Chart
+    let bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "Earth"
+      }
+    }];
 
+    let bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      xaxis: {title: "OTU ID" },
+      yaxis: {title: "Number of Bacteria" },
+      showlegend: false,
+      height: 600,
+      width: 1000
+    };
 
     // Render the Bubble Chart
-
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
-
+    let yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
 
     // Build a Bar Chart
     // Don't forget to slice and reverse the input data appropriately
+    let barData = [{
+      x: sample_values.slice(0, 10).reverse(),
+      y: yticks, 
+      text: otu_lables.slice(0, 10).reverse(),
+      type: "bar"
+      orientation: "h"
+    }];
 
+    let barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      xaxis: {title: "Number of Bacteria"},
+    };
 
     // Render the Bar Chart
-
+    Plotly.newPlot("bar", barData, barLayout);
   });
 }
 
